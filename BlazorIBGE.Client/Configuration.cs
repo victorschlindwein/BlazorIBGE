a07b1c7bb8e232e6f8db3ec96a3247b969e6f0a8
dizzysch <victorwilbert@gmail.com>
2023-12-05 17:57:02 -0300

    refactor: remove referência de api antiga

--- BlazorIBGE.Client/Configuration.cs
+++ BlazorIBGE.Client/Configuration.cs
@@ -2,7 +2,6 @@
 {
     public static class Configuration
     {
-        public static string ApiUrl { get; set; } = "http://localhost:5095";
         public static string ApiIbgeCities { get; set; } = "http://localhost:5071/ibge/GetByCity";
         public static string ApiIbgeState { get; set; } = "http://localhost:5071/ibge/GetByState";
     }
