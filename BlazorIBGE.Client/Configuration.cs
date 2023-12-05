057f11d3d7d6e99233fcb47aba5a9141e46af6f1
dizzysch <victorwilbert@gmail.com>
2023-12-05 17:54:52 -0300

    Adicionar arquivos de projeto.

---
+++ BlazorIBGE.Client/Configuration.cs
@@ -0,0 +1,9 @@
+﻿namespace BlazorIBGE
+{
+    public static class Configuration
+    {
+        public static string ApiUrl { get; set; } = "http://localhost:5095";
+        public static string ApiIbgeCities { get; set; } = "http://localhost:5071/ibge/GetByCity";
+        public static string ApiIbgeState { get; set; } = "http://localhost:5071/ibge/GetByState";
+    }
+}
---
+++ BlazorIBGE.Client/Models/CityModel.cs
@@ -0,0 +1,21 @@
+﻿namespace BlazorIBGE.Client.Models
+{
+    public class CityModel
+    {
+        public CityModel()
+        {
+
+        }
+
+        public CityModel(int id, string city, string state)
+        {
+            Id = id;
+            City = city;
+            State = state;
+        }
+
+        public int Id { get; set; }
+        public string City { get; set; }
+        public string State { get; set; }
+    }
+}
