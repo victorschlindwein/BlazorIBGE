1374990416980221ea0070a2a0fc1673a16e06f8
dizzysch <victorwilbert@gmail.com>
2023-12-07 18:19:53 -0300

    feat: add tratamento de exceção

--- BlazorIBGE.Client/Models/CityModel.cs
+++ BlazorIBGE.Client/Models/CityModel.cs
@@ -1,4 +1,6 @@
-﻿namespace BlazorIBGE.Client.Models
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace BlazorIBGE.Client.Models
 {
     public class CityModel
     {
@@ -13,9 +15,15 @@
             City = city;
             State = state;
         }
-
+        
         public int Id { get; set; }
+
+        [Required(ErrorMessage = "Informe o nome da cidade")]
+        [Length(3, 60, ErrorMessage = "A cidade deve ter entre 3 e 60 caracteres")]
         public string City { get; set; }
+
+        [Required(ErrorMessage = "Informe o nome do estado")]
+        [Length(2, 2, ErrorMessage = "O estado deve ter exatamente 2 caracteres")]
         public string State { get; set; }
     }
 }
