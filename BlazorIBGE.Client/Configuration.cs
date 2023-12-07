a01a2c4e5c7764f952727c66ccd182c2bbf12777
dizzysch <victorwilbert@gmail.com>
2023-12-07 16:30:01 -0300

    refactor: leva o campo token pra tela pro usuario informar

--- BlazorIBGE.Client/Configuration.cs
+++ BlazorIBGE.Client/Configuration.cs
@@ -6,8 +6,5 @@
         public static string GetApiIbgeState { get; set; } = "https://localhost:7267/ibge/GetByState";
         public static string GetApiIbgeId { get; set; } = "https://localhost:7267/ibge/GetById";
         public static string PostNewCity { get; set; } = "https://localhost:7267/ibge/create";
-
-        public static string TempApiAcessToken { get; set; } =
-            "Bearer eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJ1bmlxdWVfbmFtZSI6InVzZXJAZXhhbXBsZS5jb20iLCJuYmYiOjE3MDE5NzQ3MTMsImV4cCI6MTcwMjAxNzkxMywiaWF0IjoxNzAxOTc0NzEzfQ.4R0PWQdqNjiNddeA5N1zjX9EtQG_ufvdG9In4CwLHFw";
     }
 }
